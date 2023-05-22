84588c4f2819cba661dc17b0f91947084ee3c237
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-22 21:28:50 +0300

    tehtävä18

---
+++ Tehtävä18.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class Merkkijono
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Anna jokin sana tai lause, niin ohjelma tekee sille temppuja");
+        string merkkijono = Console.ReadLine();
+
+        Console.WriteLine("Alkuperäinen merkkijono:");
+        Console.WriteLine(merkkijono);
+
+        Console.WriteLine("Harvasti:");
+        TulostaHarvasti(merkkijono);
+
+        Console.WriteLine("Takaperin:");
+        TulostaTakaperin(merkkijono);
+    }
+
+    static void TulostaHarvasti(string merkkijono)
+    {
+        if (merkkijono == null)
+        {
+            Console.WriteLine("Merkkijono on null");
+            return;
+        }
+
+        int pituus = merkkijono.Length;
+
+        for (int i = 0; i < pituus; i++)
+        {
+            Console.Write(merkkijono[i]);
+
+            // Lisätään isompi väli tulostuksen välille
+            Console.Write("  ");
+        }
+
+        Console.WriteLine();
+    }
+
+    static void TulostaTakaperin(string merkkijono)
+    {
+        if (merkkijono == null)
+        {
+            Console.WriteLine("Merkkijono on null");
+            return;
+        }
+
+        int pituus = merkkijono.Length;
+
+        for (int i = pituus - 1; i >= 0; i--)
+        {
+            Console.Write(merkkijono[i]);
+        }
+
+        Console.WriteLine();
+    }
+}
