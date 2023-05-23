5da697a24ceae8179d43b7bcc22ee5bdb06cfa21
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-23 13:02:23 +0300

    tehtävä 21 ok

--- Tehtävä21/Tehtava 21.cs
+++ Tehtävä21/Tehtava 21.cs
@@ -7,6 +7,9 @@ class Nopanheitto
         // Luo Random-olio satunnaislukujen arpomista varten
         Random random = new Random();
 
+        Console.WriteLine("Ohjelma heittää kahta noppaa viisi kertaa ja laskee kaikkien heittojen yhteissumman");
+        Console.WriteLine();
+
         do
         {
             int summa = 0;
@@ -20,23 +23,15 @@ class Nopanheitto
 
                 // Lisää silmälukujen summa yhteensummaan
                 summa += silmaluku1 + silmaluku2;
-            }
-
-            // Tulosta kaikkien heittojen yhteenlaskettu summa
-            Console.WriteLine("Kaikkien heittojen yhteissumma: " + summa);
-            Console.WriteLine(); // Lisää tyhjä rivi
-
-            // Heitä noppaa uudelleen viisi kertaa ja tulosta heittojen tiedot
-            for (int i = 0; i < 5; i++)
-            {
-                // Arvo kaksi silmälukua väliltä 1-6
-                int silmaluku1 = random.Next(1, 7);
-                int silmaluku2 = random.Next(1, 7);
 
                 // Tulosta heiton tiedot
                 Console.WriteLine("Heitto {0}: Nopat ovat {1} ja {2} --> Kahden nopan summa on {3}", i + 1, silmaluku1, silmaluku2, silmaluku1 + silmaluku2);
                 Console.WriteLine(); // Lisää tyhjä rivi
             }
+
+            // Tulosta kaikkien heittojen yhteenlaskettu summa
+            Console.WriteLine("Kaikkien heittojen yhteissumma: " + summa);
+            Console.WriteLine(); // Lisää tyhjä rivi
 
             // Kysy haluatko jatkaa
             Console.Write("Haluatko jatkaa (k/e)? ");
