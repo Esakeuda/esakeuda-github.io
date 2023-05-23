d96e7cf8c03026fb8d641ac170bdc9b7e905d14f
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-23 11:56:39 +0300

    tehtävä20

---
+++ Tehtävä20/Tehtava 20.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Tehtävä20
+{
+    static void Main(string[] args)
+    {
+        bool jatka = true;
+
+        while (jatka)
+        {
+            Console.WriteLine("Syötä merkki:");
+            string syote = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(syote))
+            {
+                Console.WriteLine("Antamasi merkki on tyhjä merkki.");
+            }
+            else if (char.IsDigit(syote[0]))
+            {
+                Console.WriteLine("Antamasi merkki on numero: " + syote);
+            }
+            else if (char.IsUpper(syote[0]))
+            {
+                Console.WriteLine("Antamasi merkki on iso kirjain.");
+            }
+            else if (char.IsLower(syote[0]))
+            {
+                Console.WriteLine("Antamasi merkki on pieni kirjain.");
+            }
+            else
+            {
+                Console.WriteLine("Antamasi merkki on erikoismerkki.");
+            }
+
+            Console.WriteLine("Haluatko jatkaa? (k/e)");
+            string vastaus = Console.ReadLine();
+
+            if (vastaus.ToLower() != "k")
+            {
+                jatka = false;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
