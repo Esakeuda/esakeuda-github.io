b626226687a6280c575de6c053df14b849ebdc97
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-26 19:54:59 +0300

    Update Kirjautuminen.js

--- Demo6/Kirjautuminen.js
+++ Demo6/Kirjautuminen.js
@@ -2,5 +2,7 @@ dokument.addEventListener("DOMContentLoaded",onkoKirjautunut);
 
 function onkoKirjautunut()[
     let kirjautunut = "kyllä";
-    if(kirjautunut ---"kyllä")()
+    if(kirjautunut ---"kyllä")(
+        document.getElementById("tervetuloa_teksti").textContent += "Esa";
+    )
 ]
