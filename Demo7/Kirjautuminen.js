16b44a5bdf295e10a6d8926c212b5389c9da4b17
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-27 08:18:37 +0300

    Update Kirjautuminen.js

--- Demo7/Kirjautuminen.js
+++ Demo7/Kirjautuminen.js
@@ -1,12 +1,12 @@
 dokument.addEventListener("DOMContentLoaded",onkoKirjautunut);
 
-function onkoKirjautunut()[
+function onkoKirjautunut(){
     let kirjautunut = "kyllä";
-    if(kirjautunut === "kyllä")(
-        document.getElementById("tervetuloa_teksti").textContent += "Esa";
-    )
-]
+    if(kirjautunut === "kyllä"){
+        document.getElementById("tervetulo_teksti").textContent += "Esa";
+    }
+}
 
-function kirjaudu()[
+function kirjaudu(){
     localStorage.setItem("nimi", "Esa");
-]
+}
