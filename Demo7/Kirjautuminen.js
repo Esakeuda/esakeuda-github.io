bca8b53a0f1397308d8f62d5a76bc4b6d4dace58
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-27 07:57:22 +0300

    demo7

---
+++ Demo7/Kirjautuminen.js
@@ -0,0 +1,8 @@
+dokument.addEventListener("DOMContentLoaded",onkoKirjautunut);
+
+function onkoKirjautunut()[
+    let kirjautunut = "kyllä";
+    if(kirjautunut === "kyllä")(
+        document.getElementById("tervetuloa_teksti").textContent += "Esa";
+    )
+]
