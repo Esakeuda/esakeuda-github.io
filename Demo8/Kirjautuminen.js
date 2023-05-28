b357027a29b9da8921aa0b3daba8a5412ddbf9ee
Esakeuda <127740104+Esakeuda@users.noreply.github.com>
2023-05-28 18:26:45 +0300

    Update Kirjautuminen.js

--- Demo8/Kirjautuminen.js
+++ Demo8/Kirjautuminen.js
@@ -1,12 +1,24 @@
-document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+document.addEventListener("DOMContentLoaded", tarkistaKirjautuminen);
 
-function onkoKirjautunut() {
-  if (localStorage.getItem("kirjautunut")=== "kyllä") {
+function tarkistaKirjautuminen() {
+  if (localStorage.getItem("kirjautunut") === "kyllä") {
     document.getElementById("tervetulo_teksti").textContent += " " + localStorage.getItem("nimi");
     document.getElementById("kirjautumis_lomake").style.display = "none";
+    document.getElementById("kirjaudu_ulos_nappi").style.display = "block";
+  } else {
+    document.getElementById("kirjautumis_lomake").style.display = "block";
+    document.getElementById("kirjaudu_ulos_nappi").style.display = "none";
   }
 }
-function kirjaudu(){
+
+function kirjaudu() {
   localStorage.setItem("nimi", document.getElementById("nimi").value);
   localStorage.setItem("kirjautunut", "kyllä");
+  tarkistaKirjautuminen();
+}
+
+function kirjauduUlos() {
+  localStorage.removeItem("nimi");
+  localStorage.removeItem("kirjautunut");
+  tarkistaKirjautuminen();
 }
